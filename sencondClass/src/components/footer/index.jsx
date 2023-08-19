768c44096e2964755a8cb06e58d6e96559fe834c
estebanlarrotech <esteban.larrotech@gmail.com>
2023-08-19 23:28:36 +0100

    Adding Footer

---
+++ sencondClass/src/components/footer/index.jsx
@@ -0,0 +1,20 @@
+
+
+// import { Container } from './styles';
+
+function footer() {
+  return (
+    <div>
+      <li>
+      <ul>Home</ul>
+      <ul>About Us</ul>
+      </li>
+     <div>Contact Us</div>
+     <p>buenopues@gmail.com</p>
+    <p> <a href="https://whatsapp.com">Whatsapp</a> </p>
+     </div>
+  )
+  
+}
+
+export default footer;
