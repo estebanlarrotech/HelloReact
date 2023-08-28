cfa30a1f49f2ac9fb37def136b5cf8e329bad84f
estebanlarrotech <esteban.larrotech@gmail.com>
2023-08-28 23:35:53 +0100

    to know about styled-components

---
+++ aula006/myprofile/src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.css";
+import {styled} from "styled-components"
+
+function App() {
+ 
+const Name= styled.h1`
+color: #BF4F74;
+`;
+  return (
+    <>
+    <Name>Hello Porto</Name>
+    
+    </>
+  )
+}
+
+export default App
